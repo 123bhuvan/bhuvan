d3da7789cbb42ff9e46bd9c0bb3323aa2b764ab4
123bhuvan <92917603+123bhuvan@users.noreply.github.com>
2021-10-21 15:18:00 +0530

    Create arthamatic_operators.c

---
+++ arthamatic_operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,sum=0,sub=0,di=0,mul=0;
+    printf("enter the two numbers \n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    sub=a-b;
+    di=a/b;
+    mul=a*b;
+    printf("sum=%d \n",sum);
+    printf("sub=%d \n",sub);
+    printf("di=%d \n",di);
+    printf("mul=%d",mul);
+}    
